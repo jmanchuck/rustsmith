a9c73ce29160908225c588cd87be48b86fd493a0
jmanchuck <jjcheung0000@gmail.com>
2021-08-14 16:48:36 +0800

    move consts into single file

---
+++ smith/src/generator/consts.rs
@@ -0,0 +1,13 @@
+#![allow(dead_code)]
+pub const MAX_EXPR_DEPTH: u32 = 12;
+pub const MAX_ARITH_EXPR_DEPTH: u32 = 10;
+pub const MAX_BOOL_EXPR_DEPTH: u32 = 10;
+pub const MAX_STATICS: u32 = 2;
+pub const MAX_STRUCTS: u32 = 2;
+pub const MAX_FUNCS: u32 = 5;
+pub const MAX_FUNC_PARAMS: u32 = 6;
+
+pub const MAX_STMTS_IN_BLOCK: u8 = 8;
+pub const MAX_CONDITIONAL_BRANCHES: u8 = 2;
+pub const MAX_CONDITIONAL_DEPTH: u32 = 2; // Only refers to conditional statements
+pub const MAX_LOOP_DEPTH: u32 = 1;
