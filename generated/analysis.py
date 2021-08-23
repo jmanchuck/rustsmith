6543ff50c10118ed5bb3b6f8c65fa970d302b0b3
jmanchuck <jjcheung0000@gmail.com>
2021-08-23 16:52:00 +0800

    timeout analysis

--- generated/analysis.py
+++ generated/analysis.py
@@ -13,6 +13,7 @@ for opt_level in data:
     for seed in data[opt_level].keys():
         # NaN to indicate timed out
         if "timedout" in data[opt_level][seed]:
+            cleaned[opt_level][seed] = None
             continue
         struct_vals = list(data[opt_level][seed].values())
         checksum = sum(struct_vals)
@@ -20,9 +21,24 @@ for opt_level in data:
 
 df = pd.DataFrame.from_dict(cleaned)
 
-print(df)
+def can_differential(row):
+    return row.count() >= 2
 
-print("Rows with unequal outputs:")
+def has_differential(row):
+    if can_differential(row):
+        return np.nanmin(row) != np.nanmax(row)
+    else:
+        return False
 
-# Use NaN to indicate timed out, does not count for comparison, nanmin/max ignores nans
-print(df[df.apply(lambda x: np.nanmin(x) != np.nanmax(x), 1)])
+print("Number of timeouts:")
+print(df.isna().sum(axis=0))
+
+unequal_rows = df[df.apply(has_differential, 1)]
+
+if len(unequal_rows) == 0:
+    print("No differential found")
+else:
+    # Use NaN to indicate timed out, does not count for comparison, nanmin/max ignores nans
+    print("Rows with unequal outputs:")
+    print(unequal_rows)
+
