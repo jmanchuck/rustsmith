e95273dd8b9a584f114242826dbed2dbed38dc3a
jmanchuck <jjcheung0000@gmail.com>
2021-09-07 16:40:52 +0800

    weight update

--- smith/src/generator/weights/stmt/weights.rs
+++ smith/src/generator/weights/stmt/weights.rs
@@ -6,11 +6,11 @@ impl EnumWeights for StmtVariants {
     fn weight(&self) -> u32 {
         match self {
             StmtVariants::LetStatement => 2,
-            StmtVariants::ConditionalStatement => 0,
+            StmtVariants::ConditionalStatement => 1,
             StmtVariants::AssignStatement => 3,
-            StmtVariants::LoopStatement => 0,
+            StmtVariants::LoopStatement => 1,
             StmtVariants::OpAssignStatement => 2,
-            StmtVariants::FuncCallStatement => 3,
+            StmtVariants::FuncCallStatement => 2,
         }
     }
 }
